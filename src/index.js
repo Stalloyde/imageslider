241fe3d099fb07f03fb31d55189688067f7d3e23
Stalloyde <stalloyde93@gmail.com>
2023-01-16 21:45:40 +0800

    Complete image slider

--- src/index.js
+++ src/index.js
@@ -1,19 +1,144 @@
-import _ from "lodash";
-import printMe from "./print.js";
-import "./style.css";
+import _ from 'lodash';
+import './style.css';
 
-function component() {
-  const element = document.createElement("div");
-  const btn = document.createElement("button");
+const navBtn = document.querySelectorAll('.nav-btn');
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const img5 = document.getElementById('img5');
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+let navBarclicked = false;
+const backwardArrow = document.getElementById('arrow-backward');
+const forwardArrow = document.getElementById('arrow-forward');
+const images = [
+  {
+    name: img1,
+    zIndexJS: 1,
+  },
+  {
+    name: img2,
+    zIndexJS: 0,
+  },
+  {
+    name: img3,
+    zIndexJS: 0,
+  },
+  {
+    name: img4,
+    zIndexJS: 0,
+  },
+  {
+    name: img5,
+    zIndexJS: 0,
+  },
+];
 
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+function showDefaultImage(img) {
+  img.classList.add('appear');
 }
 
-document.body.appendChild(component());
+function showNextImage(dataKey) {
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].zIndexJS === 1) {
+      const indexOfOldImage = images.indexOf(images[i]);
+      const oldImage = images[indexOfOldImage];
+      const oldImageHTML = oldImage.name;
+      oldImage.zIndexJS = 0;
+      oldImageHTML.style.zIndex = '0';
+      oldImageHTML.classList.add('disappear');
+      oldImageHTML.classList.remove('appear');
+
+      let indexOfNextImage = indexOfOldImage + 1;
+
+      let nextImage = images[indexOfNextImage];
+      if (indexOfNextImage === images.length) {
+        indexOfNextImage = 0;
+        nextImage = images[0];
+      }
+
+      if (navBarclicked === true) {
+        indexOfNextImage = dataKey;
+        navBarclicked = false;
+      }
+
+      navBtn.forEach((btn) => {
+        const dataKey = Number(btn.getAttribute('data-key'));
+        if (dataKey === indexOfNextImage) {
+          navBtn.forEach((x) => {
+            if (x.classList.contains('clicked')) {
+              x.classList.remove('clicked');
+            }
+          });
+          btn.classList.add('clicked');
+        }
+      });
+
+      const nextImageHTML = nextImage.name;
+      nextImage.zIndexJS = 1;
+      nextImageHTML.style.zIndex = '1';
+      nextImageHTML.classList.remove('disappear');
+      nextImageHTML.classList.add('appear');
+      return;
+    }
+  }
+}
+
+function showPreviousImage() {
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].zIndexJS === 1) {
+      const indexOfOldImage = images.indexOf(images[i]);
+      const oldImage = images[indexOfOldImage];
+      const oldImageHTML = oldImage.name;
+      oldImage.zIndexJS = 0;
+      oldImageHTML.style.zIndex = '0';
+      oldImageHTML.classList.add('disappear');
+      oldImageHTML.classList.remove('appear');
+
+      let indexOfNextImage = indexOfOldImage - 1;
+      let nextImage = images[indexOfNextImage];
+
+      if (indexOfNextImage === -1) {
+        indexOfNextImage = images.length - 1;
+        nextImage = images[images.length - 1];
+      }
+
+      navBtn.forEach((btn) => {
+        const dataKey = Number(btn.getAttribute('data-key'));
+        if (dataKey === indexOfNextImage) {
+          navBtn.forEach((x) => {
+            if (x.classList.contains('clicked')) {
+              x.classList.remove('clicked');
+            }
+          });
+          btn.classList.add('clicked');
+        }
+      });
+
+      const nextImageHTML = nextImage.name;
+      nextImage.zIndexJS = 1;
+      nextImageHTML.style.zIndex = '1';
+      nextImageHTML.classList.remove('disappear');
+      nextImageHTML.classList.add('appear');
+      return;
+    }
+  }
+}
+
+showDefaultImage(img1);
+forwardArrow.addEventListener('click', showNextImage);
+backwardArrow.addEventListener('click', showPreviousImage);
+
+navBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    navBtn.forEach((x) => {
+      if (x.classList.contains('clicked')) {
+        x.classList.remove('clicked');
+      }
+    });
+    const dataKey = Number(btn.getAttribute('data-key'));
+    navBarclicked = true;
+    showNextImage(dataKey);
+    btn.classList.add('clicked');
+  });
+});
--- src/print.js
+++ src/print.js
@@ -1,3 +1,14 @@
-export default function printMe() {
-  console.log("I get called from print.js!");
+import { images } from './index';
+
+export default function hideOldImage() {
+  for (let i = 0; i < images.length; i++) {
+    const indexOfOldImage = images.indexOf(images[i]);
+    const oldImage = images[indexOfOldImage];
+    const oldImageHTML = oldImage.name;
+    oldImage.zIndexJS = 0;
+    oldImageHTML.style.zIndex = '0';
+    oldImageHTML.classList.add('disappear');
+    oldImageHTML.classList.remove('appear');
+    return indexOfOldImage;
+  }
 }
